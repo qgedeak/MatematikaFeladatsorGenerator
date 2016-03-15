2cc9c75a026117052ce78a8bdd29634e63477254
vica007 <vica007@gmail.com>
2016-03-15 12:32:53 +0100

    A megbeszéltek szerint elkészítettem az általam gondolt gombos megoldást, bár látom közben te is megcsináltad. Az enyém kicsit más lett. Nézd meg ezt is. Vica

--- MatematikaFeladatsorGenerator.py
+++ MatematikaFeladatsorGenerator.py
@@ -7,14 +7,16 @@ Created on Sun Mar  6 12:43:42 2016
 
 
 Ez egy feladat generátor lenne.
-A feladatok vegyessek , de mind a matematika témaköreiből
+A feladatok vegyesek , de mind a matematika témaköreiből
 - Egyik feladat a függvény kitalálása
 """
 
 #függvény kirajzó
 import numpy as np
 from matplotlib import pyplot as pl
+from matplotlib.widgets import Button
 import random
+
 def xnegyzet(x):
     return x*x
 def xkob(x):
@@ -33,28 +35,60 @@ fuggvenyfajtak.append([([-3,3]),'x*x*x', xkob])
 fuggvenyfajtak.append([([-3,3]),'lineáris', linearis])
 def fuggvenygen():
     return random.sample(fuggvenyfajtak ,4 )
-    
-
-pl.ion()    #külön szálra rakja a kirajzolót
-pl.show()   #így nem akasztja meg a kirajzolás a programot
 
 negy = fuggvenygen()
 ax=[0,0,0,0]
+
+#%%
+
 f, ((ax[0], ax[1]), (ax[2], ax[3])) = pl.subplots(2, 2) #mindegyikhez tennék tengelyt
 f.tight_layout() # automata elhelyezése a subplotoknak
-pl.subplots_adjust(hspace = 0.3) # a címek miatt kicsit több hely kell
+pl.subplots_adjust(hspace = 0.35, top = 0.87, bottom = 0.2) # a címek miatt kicsit több hely kell
 for i in range(4):
     ax[i].set_title(str(i+1) + " .függvény") #itt elég ha az i-t írjuk
     x = np.linspace(negy[i][0][0], negy[i][0][1], 1000)
     ax[i].plot(x, negy[i][2](x),color='r')
-
-pl.draw()
-pl.pause(0.001) #azért kell, hogy frissítse a kirajzolt képet
+    
 kiv=random.choice(negy) #elraktuk a kiválasztott függvényt
-valasz = int(input('Melyik a '+kiv[1]+' függvény? (1,2,3,4)'))-1 #a bekért választ eltettük
-if valasz>3 or valasz<0:
-    print ("A válasz 1 és 4 közötti szám lehet!")
-elif kiv==negy[valasz]: #megnézzük, hogy jó választ adott-e
-    print ("Helyes a válasz!")
-else:
-    print ("Gondold át újra!")
+f.suptitle('Melyik a '+kiv[1]+' függvény? (1,2,3,4)',fontsize=18, color='b') #a cím helyén jelenik meg a kérdés és a válasz kiértékelése is.
+
+class Index(object):
+    ind = 0
+    
+    def jo(self,valasz):
+        if kiv==negy[valasz]:
+            f.suptitle('Helyes!',fontsize=20, color='g')
+        else:
+            f.suptitle('Gondold át újra!',fontsize=20, color='r')
+
+    def egy(self, event):
+        self.jo(0)
+        pl.draw()
+
+    def ketto(self, event):
+        self.jo(1)
+        pl.draw()
+        
+    def harom(self, event):
+        self.jo(2)
+        pl.draw()
+        
+    def negy(self, event):
+        self.jo(3)
+        pl.draw()
+
+callback = Index()
+axegy = pl.axes([0.15, 0.05, 0.1, 0.075])
+axketto = pl.axes([0.35, 0.05, 0.1, 0.075])
+axharom = pl.axes([0.55, 0.05, 0.1, 0.075])
+axnegy = pl.axes([0.75, 0.05, 0.1, 0.075])
+begy = Button(axegy, '1')
+begy.on_clicked(callback.egy)
+bketto = Button(axketto, '2')
+bketto.on_clicked(callback.ketto)
+bharom = Button(axharom, '3')
+bharom.on_clicked(callback.harom)
+bnegy = Button(axnegy, '4')
+bnegy.on_clicked(callback.negy)
+
+pl.show()
